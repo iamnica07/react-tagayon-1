220dc544dd9de46c591586a6eecbaed9a0c2d105
Nica Tagayon <93661062+iamnica07@users.noreply.github.com>
2022-08-06 16:16:32 +0800

    Update Exercise1.js

--- src/pages/Exercise1.js
+++ src/pages/Exercise1.js
@@ -5,7 +5,7 @@ const Exercise1 = () => {
     return (
       <div className="text-center">
         <h1 className="mt-5">This is Exercise 1</h1>
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" onClick={() => alert("Clicked!")}>
         Click Me!
         </Button>
       </div>
