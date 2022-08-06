70264103c2954c6d336d26cd8f0e450cb483391a
Nica Tagayon <93661062+iamnica07@users.noreply.github.com>
2022-08-06 16:17:48 +0800

    Update Exercise2.js

--- src/pages/Exercise2.js
+++ src/pages/Exercise2.js
@@ -5,13 +5,13 @@ const Exercise2 = () => {
     return (
       <div className="text-center">
         <h1 className="mt-5 p-2">This is Exercise 2</h1>
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" onClick={() => alert("Button 1!")}>
         Button 1
         </Button>
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" onClick={() => alert("Button 2")}>
         Button 2
         </Button>
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" onClick={() => alert("Button 3")}>
         Button 3
         </Button>
       </div>
