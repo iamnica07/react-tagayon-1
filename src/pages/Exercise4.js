04d1e5121345d2ed837cc25e38d4402b4e7aeb86
Nica Tagayon <93661062+iamnica07@users.noreply.github.com>
2022-08-06 16:31:40 +0800

    Update Exercise4.js

--- src/pages/Exercise4.js
+++ src/pages/Exercise4.js
@@ -1,10 +1,14 @@
 const Exercise4 = () => {
   const names = ['dog', 'cat', 'chicken', 'cow', 'sheep', 'horse']
     return (
+      <>
       <div className="text-center">
         <h1 className="mt-5">This is Exercise 4</h1>
-        {names.map(name => <h2>{name}</h2>)}
+        </div>
+        <div>
+          {names.map(name => <ul><li><h4>{name}</h4></li></ul>)}
       </div>
+      </>
     )
   };
   
